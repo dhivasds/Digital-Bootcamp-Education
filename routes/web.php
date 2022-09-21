569a714872c61ee50cf11a31f224393059b60280
dhivasds <dhivasdharma@gmail.com>
2022-09-21 09:45:28 +0700

    add 'components & layout' & slicing 'checkout'

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
-Route::get('login', function () {
-    return view('login');
-})->name('login');
+Route::get('login', function () { // link url
+    return view('login'); //file login.blade.php
+})->name('login'); //name route
+
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout');
